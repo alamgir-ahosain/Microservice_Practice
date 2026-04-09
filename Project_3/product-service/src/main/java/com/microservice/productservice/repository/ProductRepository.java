69b926ba6d27224d3658c7f7aebe019d26756322
alamgir-ahosain <alamgir.ahosain@gmail.com>
2026-04-09 11:35:38 +0600

    solve producDelete issue on product-service

--- Project_3/product-service/src/main/java/com/microservice/productservice/repository/ProductRepository.java
+++ Project_3/product-service/src/main/java/com/microservice/productservice/repository/ProductRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product,String> {
     boolean existsByName(String name);
     List<Product> findByActiveTrue();
+    boolean existsByNameAndActiveTrue(String name);
 }
